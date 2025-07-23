ed46845740a9250096cb200908908dc4ce5ec9d3
Rjn-Mjn <130754848+Rjn-Mjn@users.noreply.github.com>
2025-07-23 15:09:35 +0700

    .

--- services/attendanceHandler.js
+++ services/attendanceHandler.js
@@ -225,7 +225,12 @@ async function handleAttendance({ UID, timestamp, IPAddress, Note = null }) {
     const { OTStart, OTEnd } = statusSet.recordset[0];
     console.log("[DEBUG] OTStart, OTEnd:", OTStart, OTEnd);
     if (OTStart && OTEnd) {
-      const startObj = dayjs.utc(shift.StartTime).format("YYYY-MM-DD HH:mm:ss");
+      const OTStartStr = dayjs
+        .utc(shift.StartTime)
+        .format("YYYY-MM-DD HH:mm:ss");
+      const startObj = dayjs(`${OTStartStr}`, "YYYY-MM-DD HH:mm:ss").tz(
+        "Asia/Ho_Chi_Minh"
+      );
       console.log("[DEBUG] startObj:", startObj);
       const status = startObj.isSameOrBefore(checkInEnd) ? "present" : "late";
       console.log("[DEBUG] determined status:", status);
