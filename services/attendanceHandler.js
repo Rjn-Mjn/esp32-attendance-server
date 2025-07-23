f459a65cd3bafd45bd76a64950394591a4630c99
Rjn-Mjn <130754848+Rjn-Mjn@users.noreply.github.com>
2025-07-23 12:35:45 +0700

    .

--- services/attendanceHandler.js
+++ services/attendanceHandler.js
@@ -154,9 +154,11 @@ async function handleAttendance({ UID, timestamp, IPAddress, Note = null }) {
     }
 
     // 4. Cập nhật OTEnd nếu nằm trong thời gian check-out
-    console.log(dayjs(`${scanDate}T${scanTimeStr}`));
+    console.log(dayjs(`${scanDate}T${scanTimeStr}`).utc());
 
-    console.log(dayjs(`${scanDate}T${scanTimeStr}`).isAfter(checkOutStart));
+    console.log(
+      dayjs(`${scanDate}T${scanTimeStr}`).utc().isAfter(checkOutStart)
+    );
 
     if (!shift.OTEnd && scanTime.isAfter(checkOutStart)) {
       await pool
