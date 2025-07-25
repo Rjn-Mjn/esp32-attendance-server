3452bd6aaf94d539441a9a0f97a752131834e0e2
Rjn-Mjn <130754848+Rjn-Mjn@users.noreply.github.com>
2025-07-25 09:45:24 +0700

    .

--- services/attendanceHandler.js
+++ services/attendanceHandler.js
@@ -239,7 +239,7 @@ async function handleAttendance({ UID, timestamp, IPAddress, Note = null }) {
         .request()
         .input("AccountID", sql.VarChar(100), AccountID)
         .input("ShiftID", sql.VarChar(100), shift.ShiftID)
-        .input("OTEnd", sql.DateTime, timeScanned)
+        .input("OTEnd", sql.VarChar, timeScanned)
         .query(
           `UPDATE Attendance SET OTEnd = @OTEnd WHERE AccountID = @AccountID AND ShiftID = @SHIFTID`
         );
